ca115135ce1b9ffa48e43ba37da63024242b1914
salimess <salim1999yee@gmail.com>
2023-03-16 15:22:06 -0700

    Hello, world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ int main(void)
         printf("Size of a long int: %d byte(s)\n", sizeof(long int));
         printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
         printf("Size of float: %d byte(s)\n", sizeof(float));
-        return 0;
+        return (0);
 }
